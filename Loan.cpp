def84d20808bcf4f041d0c8269dba685af5d0b48
shhh-9 <shahmeerabdullah83@gmail.com>
2024-12-08 16:41:22 +0500

    New Function

--- Loan.cpp
+++ Loan.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
-int main(){
-       cout<<"Hello";
-    return 0;
+int Loan(){
+
+    int loan;
+    cout<<"How much loan do u want from the bank?"<<endl;
+    cin>>loan;
+
+    int date;
+    cout<<"when will you be paying back the loan?"<<endl;
+    cin>>date;
+
+    int method;
+    cout<<"How will you be paying back the loan [Monthly Installemnets] or [Whole at Once]?"<<endl;
+    cin>>method;
+
+    
+    
+
 }
